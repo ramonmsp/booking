8ace548b5247f4341329f9149cfe320404f79855
Ramon Maciel  Sousa Prado <ramon.prado@hp.com>
2024-03-25 14:44:54 -0300

    feat: edit existing booking

---
+++ src/app/bookings/edit/[bookingId]/page.tsx
@@ -0,0 +1,84 @@
+'use client';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import BookingForm, {
+  DatePickedRange,
+} from '@/app/components/booking_form/page';
+import { editBooking, getBookings } from '@/app/utils/requests';
+import { Booking } from '@/app/lib/mocks/booking';
+
+export type DateRange = {
+  start: Date;
+  end: Date;
+};
+
+export type BookDate = {
+  bookingDates: DateRange;
+};
+
+type EditBookingProps = {
+  params: {
+    bookingId: string;
+  };
+};
+
+const EditBooking = ({ params }: EditBookingProps) => {
+  const [open, setOpen] = React.useState(false);
+  const router = useRouter();
+  const { bookingId } = params;
+
+  const [placeToBook, setPlaceToBook] = React.useState<
+    Booking | NonNullable<unknown>
+  >({});
+  const [bookings, setBookings] = React.useState<Booking[]>([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const bookings = await getBookings();
+        const booking = bookings?.find(
+          ({ bookingId }: { bookingId: string }) => bookingId === bookingId,
+        );
+
+        setPlaceToBook(booking);
+        setBookings(
+          bookings.filter(({ id }: { id: string }) => id !== bookingId),
+        );
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [bookingId, setPlaceToBook, setBookings]);
+
+  const handleOpen = React.useCallback((open: boolean) => {
+    setOpen(open);
+  }, []);
+
+  const handleCancel = React.useCallback(() => {
+    setOpen(false);
+    router.push('/bookings');
+  }, [router]);
+
+  const onFinish = async (values: DatePickedRange) => {
+    await editBooking(values, bookingId);
+    setOpen(false);
+    router.push('/bookings');
+  };
+
+  return (
+    <BookingForm
+      placeToBook={placeToBook!}
+      bookings={bookings ?? []}
+      onCancel={handleCancel}
+      onFinish={onFinish}
+      open={open}
+      handleOpen={handleOpen}
+      modalTitle="Update your booking dates: "
+      submitButtonLabel="Update booking"
+    />
+  );
+};
+
+export default EditBooking;
