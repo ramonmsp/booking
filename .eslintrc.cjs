582aa17177a01c4692e0669b6e16b6eaddc91fa7
Ramon Maciel  Sousa Prado <ramon.prado@hp.com>
2024-03-27 18:13:33 -0300

    feat: add cypress configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,16 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, "jest": true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:jest/recommended',
     'plugin:react-hooks/recommended',
     'plugin:jest-dom/recommended', 
-    'plugin:testing-library/react'
+    'plugin:testing-library/react',
+    'plugin:cypress/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'tests/e2e/*.js'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
