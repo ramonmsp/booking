336080f210f3b6b107e39f19686cfcf204ee3e2b
Ramon Maciel  Sousa Prado <ramon.prado@hp.com>
2024-03-27 18:13:00 -0300

    chore: rename setup jest file

--- jest.config.ts
+++ jest.config.ts
@@ -141,7 +141,7 @@ const config: Config = {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: [
-    '<rootDir>/src/test/setup.ts'
+    '<rootDir>/src/test/setupTests.ts'
   ],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
